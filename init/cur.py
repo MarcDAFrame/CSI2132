9f55af35bd7451e30f82e6fde7d0c6ab45d44c89
mframe <mframe45@gmail.com>
2020-04-08 20:02:27 -0400

    working on geting docker to work w/ it

--- init/cur.py
+++ init/cur.py
@@ -2,7 +2,7 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT # <-- ADD THIS LINE
 
 try:
-    conn = psycopg2.connect(dbname='postgres', host='localhost', port='54320', user='postgres')
+    conn = psycopg2.connect(dbname='postgres', host='localhost', port='5432', user='postgres')
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 except Exception as e:
     print(e)
